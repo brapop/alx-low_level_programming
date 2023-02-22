3f21abdc5d4f742d32807d198c5a999487bae03c
JONATHAN AHAMAH <brapop123@gmail.com>
2023-02-21 16:14:38 -0800

    done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
-#ifndef main_c
-#define maim_c
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
-void print _alphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -12,7 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n)
+void print_to_98(int n);
 void print_times_table(int n);
 
-#endif 
+#endif
