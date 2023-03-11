65917255217e36f702b8d0f212b7767e65d46971
JONATHAN AHAMAH <brapop123@gmail.com>
2023-03-11 13:30:46 -0800

    not easy one

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -3,10 +3,9 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- *
- * Return: void	
+ * Return: void
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src);
 {
 	int i;
 	int j;
@@ -23,7 +22,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
+	}
 
 
 	dest[i] = '\0';
